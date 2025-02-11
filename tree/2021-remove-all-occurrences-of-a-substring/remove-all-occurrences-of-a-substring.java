93965d37047b58ed49241c3a547903622f051c3b
Ajay Singh <ajaylion12345@gmail.com>
2025-02-11 12:47:33 +0530

    Time: 1 ms (99.46%) | Memory: 42 MB (88.37%) - LeetSync

--- tree/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ tree/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,8 +1,31 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
-        while(s.contains(part)){
-            s=s.replaceFirst(part,"");
+        // while(s.contains(part)){
+        //     s=s.replaceFirst(part,"");
+        // }
+        // return s;
+
+        char[] input=s.toCharArray();
+        char[] tar=part.toCharArray();
+        int tl=tar.length;
+        char[] res=new char[input.length];
+        int stackSize=0;
+        int tarEndchar=tar[tl-1];
+
+        for(char c:input){
+            res[stackSize++]=c;
+
+            if(c==tarEndchar && stackSize>=tl){
+                int j=tl-1;int i=stackSize-1;
+                while(j>=0 && res[i]==tar[j]){
+                    i--;
+                    j--;
+                }
+                if(j<0){
+                    stackSize=i+1;
+                }
+            }
         }
-        return s;
+        return new String(res,0,stackSize);
     }
 }
