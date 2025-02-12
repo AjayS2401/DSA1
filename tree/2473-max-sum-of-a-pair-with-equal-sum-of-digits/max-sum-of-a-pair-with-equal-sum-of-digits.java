bf45b4a378a18dcb205a8102d2c14b6c4d220955
Ajay Singh <ajaylion12345@gmail.com>
2025-02-12 12:01:25 +0530

    Time: 61 ms (30.23%) | Memory: 63.6 MB (7.73%) - LeetSync

--- tree/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ tree/2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,33 +1,57 @@
 class Solution {
-    int getdigit(int n){
-        // while(n>=10){
-            int sum=0;
-            while(n>0){
-                sum+=n%10;
-                n=n/10;
-            }
+    // int getdigit(int n){
+    //     // while(n>=10){
+    //         int sum=0;
+    //         while(n>0){
+    //             sum+=n%10;
+    //             n=n/10;
+    //         }
            
-        // }
+    //     // }
+    //     return sum;
+    // }
+    // public int maximumSum(int[] nums) {
+    //     Map<Integer,List<Integer>> hm=new HashMap<>();
+    //     for(int n:nums){
+    //         int dig=getdigit(n);
+    //         hm.computeIfAbsent(dig,key->new ArrayList()).add(n);
+    //     }
+    //     int maxS=-1;
+
+    //     // for(Map.Entry<Integer,List<Integer>> entry:hm.entrySet())
+    //     for(List<Integer> al:hm.values()){
+    //         if(al.size()>=2){
+    //             al.sort((a,b)->b-a);
+    //             int sum=al.get(0)+al.get(1);
+    //             if(sum>maxS)
+    //             maxS=sum;
+    //         }
+            
+    //     }
+    //     return maxS;
+     private int getSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += (n % 10);
+            n /= 10;
+        }
         return sum;
     }
-    public int maximumSum(int[] nums) {
-        Map<Integer,List<Integer>> hm=new HashMap<>();
-        for(int n:nums){
-            int dig=getdigit(n);
-            hm.computeIfAbsent(dig,key->new ArrayList()).add(n);
-        }
-        int maxS=-1;
 
-        // for(Map.Entry<Integer,List<Integer>> entry:hm.entrySet())
-        for(List<Integer> al:hm.values()){
-            if(al.size()>=2){
-                al.sort((a,b)->b-a);
-                int sum=al.get(0)+al.get(1);
-                if(sum>maxS)
-                maxS=sum;
-            }
-            
+    public int maximumSum(int[] nums) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for (int num : nums) {
+            int sum = getSum(num);
+            map.putIfAbsent(sum, new PriorityQueue<>(Collections.reverseOrder()));
+            map.get(sum).offer(num);
         }
-        return maxS;
+
+        long maxSum = -1;
+        for (PriorityQueue<Integer> pq : map.values()) {
+            if (pq.size() < 2) continue;
+            long sum = pq.poll() + pq.poll();
+            maxSum = Math.max(maxSum, sum);
+        }
+        return (int) maxSum;
     }
 }
