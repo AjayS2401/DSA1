bfd359f754fda3bd0175fb1be2399543d4c24273
Ajay Singh <ajaylion12345@gmail.com>
2025-02-18 15:04:25 +0530

    Time: 1 ms (75.25%) | Memory: 41.9 MB (36.89%) - LeetSync

--- tree/2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
+++ tree/2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.java
@@ -1,23 +1,38 @@
 class Solution {
     public String smallestNumber(String pattern) {
-        StringBuilder sb=new StringBuilder();
-        getseq(0,0,pattern.toCharArray(),sb);
+        // StringBuilder sb=new StringBuilder();
+        // getseq(0,0,pattern.toCharArray(),sb);
 
-        return sb.reverse().toString();
-    }
+        // return sb.reverse().toString();
 
-    int getseq(int currI,int currCount,char[] pat,StringBuilder res){
 
-        if(currI !=pat.length){
-            if(pat[currI]=='I'){
-                getseq(currI+1,currI+1,pat,res);
-            }else {
-                currCount=
-                getseq(currI+1,currCount,pat,res);
+        //Usign stack
+
+        StringBuilder res=new StringBuilder();
+        Stack<Integer> st=new Stack<>();
+        for(int idx=0;idx<=pattern.length();idx++){
+            st.push(idx+1);
+            if(idx==pattern.length() || pattern.charAt(idx)=='I'){
+                while(!st.isEmpty()){
+                    res.append(st.pop());
+                }
             }
-            
         }
-        res=res.append(currCount+1);
-        return currCount+1;
+        return res.toString();
     }
+
+    // int getseq(int currI,int currCount,char[] pat,StringBuilder res){
+
+    //     if(currI !=pat.length){
+    //         if(pat[currI]=='I'){
+    //             getseq(currI+1,currI+1,pat,res);
+    //         }else {
+    //             currCount=
+    //             getseq(currI+1,currCount,pat,res);
+    //         }
+            
+    //     }
+    //     res=res.append(currCount+1);
+    //     return currCount+1;
+    // }
 }
