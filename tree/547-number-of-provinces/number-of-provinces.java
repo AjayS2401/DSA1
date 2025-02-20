4c774afd5b963a2feab6c3b7d585b007c66489d7
Ajay Singh <ajaylion12345@gmail.com>
2025-02-20 22:38:29 +0530

    Time: 1 ms (88.21%) | Memory: 48.4 MB (13.93%) - LeetSync

--- tree/547-number-of-provinces/number-of-provinces.java
+++ tree/547-number-of-provinces/number-of-provinces.java
@@ -5,34 +5,41 @@ class Solution {
         for(int i=0;i<n;i++){
             adj.add(new ArrayList());
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-            if(isConnected[i][j]==1 && i!=j){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //     if(isConnected[i][j]==1 && i!=j){
 
-            adj.get(i).add(j);
-            adj.get(j).add(i);
-            }
-            }
-        }
+        //     adj.get(i).add(j);
+        //     adj.get(j).add(i);
+        //     }
+        //     }
+        // }
         boolean []visit=new boolean[n];
         Arrays.fill(visit,false);
         int count=0;
         for(int i=0;i<n;i++){
                 if(!visit[i]){
                      count++;
-                    DFSG(adj,visit,i);
+                    // DFSG(adj,visit,i);
+                      DFSG(isConnected,visit,i);
                 }
                
         }
         return count;
 
     }
-    void DFSG(ArrayList<ArrayList<Integer>> adj,boolean[] visit,int u){
+    // void DFSG(ArrayList<ArrayList<Integer>> adj,boolean[] visit,int u){
+     void DFSG(int[][] isConnected,boolean[] visit,int u){
         visit[u]=true;
-
-        for(int v:adj.get(u)){
-            if(!visit[v])
-            DFSG(adj,visit,v);
+        for(int v=0;v<isConnected.length;v++){
+            if(isConnected[u][v]==1 && !visit[v]){
+                DFSG(isConnected,visit,v);
+            }
         }
+
+        // for(int v:isconnected[u]){
+        //     if(!visit[v])
+        //     DFSG(adj,visit,v);
+        // }
     }
 }
